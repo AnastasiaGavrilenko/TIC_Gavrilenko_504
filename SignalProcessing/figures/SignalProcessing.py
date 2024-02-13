08dc42addd6a53ba1d1bcbf1f301dec62589fc4d
AnastasiaGavrilenko <a.a.havrylenko@student.khai.edu>
2024-02-13 13:47:22 +0200

    Фильтрация сигнала

--- SignalProcessing/figures/SignalProcessing.py
+++ SignalProcessing/figures/SignalProcessing.py
@@ -1,15 +1,22 @@
+import numpy as np
 from scipy import signal
-# Параметри фільтру та сигналу
+
+mean = 0
+std_deviation = 10
+n = 500  # Довжина сигналу у відліках
+Fs = 1000  # Частота дискретизації
+
+# Генерація випадкового сигналу
+random_signal = np.random.normal(mean, std_deviation, n)
+
+# Визначення параметрів фільтру
 order = 3  # порядок фільтру
 F_max = 5  # максимальна частота сигналу в Гц
-Fs = 1000  # частота дискретизації в Гц
-
-# Нормування частоти
 w = F_max / (Fs / 2)
-
-# Розрахунок параметрів фільтру низьких частот
 b, a = signal.butter(order, w, 'low', output='ba')
 
+# Фільтрація сигналу
+filtered_signal = signal.sosfiltfilt(signal.butter(order, w, 'low', output='sos'), random_signal)
+
 # Результат
-print("Коефіцієнти чисельника (b):", b)
-print("Коефіцієнти знаменника (a):", a)
+print("Фільтрований сигнал:", filtered_signal)
